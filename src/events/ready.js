88a2f7b4245b5f510ee8da1919b06671de71cd13
Mixeleoo <superrapace531@gmail.com>
2026-04-25 22:16:28 +0200

    Oubli de l'ancien commit, ajout des commandes "chat" et "trust" dans ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -2,7 +2,7 @@ export default function readyHandler(client) {
   client.once("ready", async () => {
     console.log(`${client.user.tag} prêt à discuter !`);
     client.user.setActivity("Discute avec les utilisateurs !");
-    
+
     try {
       const commands = [
         {
@@ -12,11 +12,35 @@ export default function readyHandler(client) {
         {
           name: "help",
           description: "Affiche l'aide et explique le fonctionnement du bot"
+        },
+        {
+          name: "trust",
+          description: "Indique ton score de fiabilité",
+          options: [
+            {
+              name: "user",
+              type: "USER",
+              description: "L'utilisateur dont tu veux connaître le score",
+              required: true
+            }
+          ]
+        },
+        {
+          name: "chat",
+          description: "Engage une conversation avec le bot",
+          options: [
+            {
+              name: "chat",
+              type: "STRING",
+              description: "La question que tu veux poser",
+              required: true
+            }
+          ]
         }
       ];
 
       await client.application.commands.set(commands);
-      console.log("Commandes slash (/maj, /help) enregistrées !");
+      console.log("Commandes slash (/maj, /help, /trust, /question) enregistrées !");
     } catch (error) {
       console.error("Erreur lors de l'enregistrement des commandes:", error);
     }
